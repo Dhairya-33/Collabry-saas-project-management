4075eb22c7f7c370739784d6b3dc0252b47ed5cd
dhairya-33 <dhairyamaru23@gmail.com>
2026-01-06 00:02:48 +0530

    refactor(models): fix project member constraints and enhance user auth internals

--- backend/src/models/projectmember.model.js
+++ backend/src/models/projectmember.model.js
@@ -5,19 +5,16 @@ const projectMember = new Schema({
         type : Schema.Types.ObjectId,
         ref:"User",
         required:true,
-        unique:true
     },
     projectId:{
         type : Schema.Types.ObjectId,
         ref:"Project",
-        required:true,
         required:true,
     },
     companyId:{
         type:Schema.Types.ObjectId,
         ref:"Company",
         required:true,
-        unique:true
     },
     role: { type: String, enum: ["admin", "manager", "employee"], default: "employee" },
   
--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose,{Schema} from 'mongoose'
+import  jwt  from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
 import { configDotenv } from "dotenv";
 configDotenv({
       path:'./.env'
@@ -54,5 +56,50 @@ const userSchema = new Schema({
         unique:false
     }
 },{timestamps:true});
+
+
+userSchema.pre("save", async function (next) {
+    if(!this.isModified("password")) return next();
+
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+})
+
+
+
+userSchema.methods.generateAccessTKN = async function (req,res){
+    return jwt.sign(
+        {
+            _id : this._id,
+            username:this.username,
+            email:this.email,
+            fullname:this.fullname
+        },
+        process.env.ACCESSTKN_KEY,
+        {
+            expiresIn:process.env.ACCESSTKN_EXPIRESIN
+        }
+    )
+}
+userSchema.methods.generateRefreshTKN = async function (req,res){
+    return jwt.sign(
+        {
+            _id : this._id,
+            username:this.username,
+            email:this.email,
+            fullname:this.fullname
+        },
+        process.env.REFRESHTKN_KEY,
+        {
+            expiresIn:process.env.REFRESHTKN_EXPIRESIN
+        }
+    )
+}
+
+userSchema.methods.verifyPassword = async function(oldPassword) {
+    return await bcrypt.compare(oldPassword,this.password);
+}
+
+
 
 export const User = mongoose.model("User",userSchema)
