a1c41ead087ec6a8347c57f562bb095bc1c1e30a
dhairya-33 <dhairyamaru23@gmail.com>
2026-01-28 23:48:36 +0530

    chore: set up React frontend and Tailwind configuration

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+
+  return (
+    <>
+      <div 
+      className='bg-red-400 text-white text-center'>check tailwind css</div>
+    </>
+  )
+}
+
+export default App
